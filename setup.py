ac061e1c5c0bb2d4f1fb6948576ec29cf281fec7
Simone <simone.maccio2@gmail.com>
2023-02-28 18:03:28 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'sofar_crane_simulator'
+lib = 'sofar_crane_simulator/lib'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name, lib],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "resource"), glob("resource/*.png")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py"))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='simone',
+    maintainer_email='simone.maccio@edu.unige.it',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'crane_sim_node = sofar_crane_simulator.crane_sim_node:main', 
+        ],
+    },
+)
